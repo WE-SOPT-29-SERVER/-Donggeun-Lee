06e82a95e2b6e625ffa10f2be15e899d0b936c46
donggeun <geeneve@naver.com>
2021-10-27 22:27:55 +0900

    [FEAT]: post router 기능 추가

---
+++ 3rd-seminar/level1-2/routes/post/postPostsGET.js
@@ -0,0 +1,18 @@
+/*
+get posts
+METHOD: GET
+URI: localhost:3000/post/
+RESPONSE STATUS: 200 (OK)
+RESPONSE DATA: DB에 있는 모든 게시물들
+*/
+
+const { fail, success } = require('../../lib/util');
+const responseMessage = require('../../constants/responseMessage');
+const statusCode = require('../../constants/statusCode');
+
+const posts = require('../../dbMockup/post');
+
+module.exports = async (req, res) => {
+  // DB의 모든 post를 response
+  res.status(statusCode.OK).send(success(statusCode.OK, responseMessage.GET_POST_SUCCESS, posts));
+};
