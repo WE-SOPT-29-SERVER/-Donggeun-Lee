7dc1ffa0089d920681a617b4bc29678af18b007c
donggeun <geeneve@naver.com>
2021-12-23 17:03:53 +0900

    add: lib, constants 추가

--- 3rd-seminar/level1-2/routes/user.js
+++ 3rd-seminar/level1-2/routes/user.js
@@ -2,25 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 const users = require('../dbMockup/user');
+const util = require('../lib/util');
 
 router.post('/signup', async (req, res) => {
   const { id, name, password, email } = req.body;
 
   // data null check
   if (!id || !name || !password || !email) {
-    return res.status(400).send({ status: 400, message: 'BAD REQUEST' });
+    return res.status(400).send(util.fail(400, 'BAD REQUEST'));
   }
 
   const exist = users.filter((user) => user.email === email).length > 0;
   if (exist) {
-    return res.status(409).send({ status: 409, message: 'USED EMAIL' });
+    return res.status(409).send(util.fail(409, 'USED EMAIL'));
   }
 
   const newUser = { id, name, password, email };
 
   users.push(newUser);
 
-  res.status(200).send(newUser);
+  res.status(200).send(util.success(200, '회원가입 성공', newUser));
 });
 
 module.exports = router;
